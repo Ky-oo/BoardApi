dfbad57d25dc4a7a0aab88824697e95362b4d3b1
Ky-oo <kylian.patry@gmail.com>
2025-12-14 21:19:01 +0100

    Update home route to return JSON response with welcome message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.json({
+    title:
+      "Bienvenue sur BoardApi. Merci de lire le README.md pour plus d'informations.",
+  });
 });
 
 module.exports = router;
